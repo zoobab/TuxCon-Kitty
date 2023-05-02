cb00909068cbf62b837d0a5db259c87b35235c30
Benjamin Henrion <zoobab@gmail.com>
2023-05-02 15:34:30 +0200

    Update morse.ino
    add morse code ref

--- SOFTWARE/Morse_Code/morse.ino
+++ SOFTWARE/Morse_Code/morse.ino
@@ -1,10 +1,42 @@
 // This displays the name BELLA in morse code
 // on the Olimex Kitty badge
-// See the morse code reference here https://math.hws.edu/eck/cs225/s03/code.txt
+// Morse code:
+// A .- || B -... || C -.-. || D -.. || E . || F ..-. || G --. ||  H .... || I .. || J .--- ||
+// K -.- || L .-.. || M -- || N -. || O --- || P .--. || Q --.- || R .-. || S ... || T - || 
+// U ..- || V ...- || W .-- || X -..- || Y -.-- || Z --.. ||
+// 1 .---- || 2 ..--- || 3 ...-- || 4 ....- || 5 ..... || 6 -.... || 7 --... || 8 ---.. ||
+// 9 ----. || 0 ----- ||
+//  -.-.-.-
+//, --..--
+//: ---...
+//; -.-.-.
+//. .-.-.-
+//" .-..-.
+//( -----.
+//) .-----
+//' -.--.-
 
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
+}
+
+# short, or blink for half a second
+void s()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
+
+# long, or blink for 2 seconds
+void l()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
 
 void loop()
@@ -19,20 +51,4 @@ void loop()
   delay(2000);
   s(); l(); //A
   delay(10000);
-}
-
-void s()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-}
-
-void l()
-{
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(2000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
 }
